98385be2c356ebe954b0b7efa9a29fbe73edcb44
cucereanum <cucereanum@yahoo.com>
2023-01-30 22:24:01 +0200

    feat: create a reusable heading-with-background component

---
+++ src/lib/headings/HeadingWithBackground.svelte
@@ -0,0 +1,92 @@
+<script>
+	export let headingImageUrl, headingText, subHeadingText;
+</script>
+
+<div class="container" style="--headingImageUrl: url({headingImageUrl})">
+	<div>
+		<h1>{headingText}</h1>
+		<p>{subHeadingText}</p>
+	</div>
+</div>
+
+<style>
+	.container {
+		width: 100%;
+		height: 100%;
+		background-image: var(--headingImageUrl);
+		background-size: cover;
+		background-position: center;
+		display: flex;
+	}
+	.container div {
+		background-color: rgb(34, 34, 34);
+		width: 40rem;
+		height: 30rem;
+		align-self: flex-end;
+		margin-left: 10%;
+		color: #fff;
+		text-align: center;
+		padding: 2em;
+		border-radius: 1em;
+	}
+	.container h1 {
+		font-size: 3.2rem;
+		letter-spacing: 0.2em;
+		margin: 0;
+	}
+	.container p {
+		font-size: 1.4rem;
+		line-height: 2.2rem;
+	}
+	@media screen and (min-width: 1024px) and (max-width: 1475px) {
+		.container div {
+			height: 25rem;
+			width: 30rem;
+		}
+		.container h1 {
+			font-size: 2.2rem;
+		}
+		.container p {
+			font-size: 1.1rem;
+			line-height: 1.8rem;
+		}
+	}
+	@media screen and (min-width: 680px) and (max-width: 1023px) {
+		.container {
+			justify-content: center;
+		}
+		.container div {
+			align-self: flex-end;
+			width: 75%;
+			height: 24em;
+			margin-left: 0;
+			margin-bottom: 5%;
+		}
+		.container p {
+			font-size: 1.2rem;
+			line-height: 1.6rem;
+		}
+		.container h1 {
+			font-size: 1.8rem;
+		}
+	}
+	@media screen and (max-width: 679px) {
+		.container {
+			justify-content: center;
+		}
+		.container div {
+			align-self: flex-end;
+			width: 75%;
+			height: 24em;
+			margin-left: 0;
+			margin-bottom: 10%;
+		}
+		.container p {
+			font-size: 0.8rem;
+			line-height: 1.3rem;
+		}
+		.container h1 {
+			font-size: 1.8rem;
+		}
+	}
+</style>
---
+++ src/routes/cine-suntem/viziune/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Fullpage, FullpageSection } from "svelte-fullpage";
+	import HeadingWithBackground from "$lib/headings/HeadingWithBackground.svelte";
+
+	let headingImageUrl = "/images/939-1920x1080.webp";
+	let headingText = "ARDOR Muntenia";
+	let subHeadingText =
+		"În cadrul societății, ARDOR Muntenia își asumă misiunea de a contribui activ la educarea tinerilor pentru o cultură a dialogului și a dezbaterii în România. Printre valorile la care asociația aderă, se numără profesionalismul, toleranța și echitatea, acestea regăsindu-se atât în relația cu beneficiarii direcți ai dezbaterilor cât și cu celelalte asociații regionale ARDOR din țară.Asociatia Regionala de Dezbateri, Oratorie si Dezbateri – Muntenia (ARDOR Muntenia) este entitatea care gestioneaza activitatea de dezbateri in spatiul geografic al regiunii istorice Muntenia.";
+</script>
+
+<Fullpage>
+	<FullpageSection title="Viziune">
+		<HeadingWithBackground {headingImageUrl} {headingText} {subHeadingText} />
+	</FullpageSection>
+</Fullpage>
