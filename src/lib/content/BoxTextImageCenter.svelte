6c301f654454755f5c199b1773373fd2eb74eada
cucereanum <cucereanum@yahoo.com>
2023-02-01 21:33:35 +0200

    feat: create a box-with-two-images component

--- src/lib/content/BoxTextImageCenter.svelte
+++ src/lib/content/BoxTextImageCenter.svelte
@@ -1,12 +1,13 @@
 <script>
-	import HeadingWithBackground from "../headings/HeadingWithBackground.svelte";
-
 	export let subHeadingText, headingText, headingImageUrl;
 </script>
 
 <div class="container">
+	<div class="content">
+		<h1>{headingText}</h1>
+		<h3>{subHeadingText}</h3>
+	</div>
 	<img src={headingImageUrl} alt="Beautiful people" />
-	<HeadingWithBackground {headingText} {subHeadingText} headingImageUrl="" />
 </div>
 
 <style>
@@ -19,17 +20,60 @@
 		align-items: center;
 	}
 	.container img {
-		width: 20em;
-		height: 20em;
+		width: 15em;
+		height: 30%;
 		object-fit: contain;
-		margin-top: 2.1em;
-		border-radius: 1em;
+	}
+	.content {
+		background-color: #3b4048;
+		width: 18em;
+		height: 14em;
+		text-align: center;
+		color: #fff;
+		padding: 1em;
+		margin-top: 1em;
+	}
+	h3 {
+		font-size: 0.6rem;
+	}
+	h1 {
+		font-size: 1.6rem;
+		margin: 0;
+	}
+	@media screen and (min-width: 680px) and (max-width: 1023px) {
+		.content {
+			width: 70%;
+		}
+		.container img {
+			width: 50%;
+			height: 15em;
+		}
+		h3 {
+			font-size: 0.8rem;
+			letter-spacing: 1px;
+		}
 	}
 	@media screen and (min-width: 1024px) {
 		.container {
 			flex-direction: row;
 			justify-content: center;
 			align-items: center;
+		}
+		.content {
+			width: 30em;
+			height: 25em;
+		}
+		.container img {
+			width: 30em;
+			height: 20em;
+		}
+		h3 {
+			font-size: 1rem;
+			letter-spacing: 1px;
+		}
+		h1 {
+			font-size: 2.4rem;
+			margin: 0.5em 0 0 0;
 		}
 	}
 </style>
---
+++ src/routes/cum-fac-dezbateri/clubul/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { Fullpage, FullpageSection } from "svelte-fullpage";
+	import BoxTextTwoImages from "$lib/content/BoxTextTwoImages.svelte";
+
+	let imageOneUrl = "/images/elevi.webp";
+	let imageTwoUrl = "/images/instructori.webp";
+	let titleOne = "Elevi";
+	let titleTwo = "Instructori";
+	let descOne =
+		"Probabil că dezbaterile sunt cea mai bună unealtă prin care poți să îți impresionezi profesorii, prietenii și potențialii angajatori. Noi îți oferim oportunitatea să te dezvolți pe plan personal și profesional într-un cadru academic, dar și să te distrezi în timp ce faci acest lucru.";
+	let descTwo =
+		"Oferim posibilitatea atât elevilor cât și profesorilor să învețe mai multe despre artă dezbaterii. În acest sens, noi oferim cursuri instructorilor de dezbateri în care aceștia învață cum să predea tinerilor debateri tehnică argumentării și oratoriei.";
+</script>
+
+<Fullpage>
+	<FullpageSection name="Clubul">
+		<BoxTextTwoImages {imageOneUrl} {imageTwoUrl} {titleOne} {titleTwo} {descOne} {descTwo} />
+	</FullpageSection>
+</Fullpage>
