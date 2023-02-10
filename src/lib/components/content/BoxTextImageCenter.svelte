9486abcae2d7e2fab32d7b04439f1cd74c6c98eb
Tech-Dex <rominu.alexandru@yahoo.com>
2023-02-10 19:20:34 +0200

    chore: center content

--- src/lib/components/content/BoxTextImageCenter.svelte
+++ src/lib/components/content/BoxTextImageCenter.svelte
@@ -18,62 +18,77 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
+		justify-content: center;
 	}
+
 	.container img {
 		width: 15rem;
 		height: 30%;
 		object-fit: contain;
 	}
+
 	.content {
 		background-color: #3b4048;
 		width: 18rem;
 		height: 14rem;
-		text-align: center;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
 		color: #fff;
 		padding: 1rem;
 		margin-top: 1rem;
 	}
+
 	h3 {
 		font-size: 0.6rem;
 		font-weight: 300;
 	}
+
 	h1 {
 		font-size: 1.6rem;
 		font-weight: 700;
 		margin: 0;
 	}
+
 	@media screen and (min-width: 680px) and (max-width: 1023px) {
 		.content {
 			width: 70%;
 		}
+
 		.container img {
 			width: 50%;
 			height: 15rem;
 		}
+
 		h3 {
 			font-size: 0.8rem;
 			letter-spacing: 1px;
 		}
 	}
+
 	@media screen and (min-width: 1024px) {
 		.container {
 			flex-direction: row;
 			justify-content: center;
 			align-items: center;
 		}
+
 		.content {
 			width: 30rem;
 			height: 25rem;
 		}
+
 		.container img {
 			width: 30rem;
 			height: 20rem;
 		}
+
 		h3 {
 			font-size: 1.2rem;
-
 			letter-spacing: 1px;
 		}
+
 		h1 {
 			font-size: 2.4rem;
 
