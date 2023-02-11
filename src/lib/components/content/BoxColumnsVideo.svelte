9929be050fdb47fcded1f8611845d4c35ca48a07
Tech-Dex <rominu.alexandru@yahoo.com>
2023-02-11 18:52:55 +0200

    fix: video & responsive

--- src/lib/components/content/BoxColumnsVideo.svelte
+++ src/lib/components/content/BoxColumnsVideo.svelte
@@ -12,7 +12,6 @@
 		camera: camera,
 		headphones: headphones,
 	};
-	console.log(beneficiiColumns);
 </script>
 
 <div class="container">
@@ -28,26 +27,59 @@
 			</div>
 		{/each}
 	</div>
-
-	<iframe
-		class="video"
-		src="https://www.youtube.com/embed/3z37Iy3JvB0?autoplay=1&mute=1&controls=0&rel=0"
-		title="Tabăra de leadership pentru profesori și instructori de dezbateri (ediția 2018)"
-		frameborder="0"
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-		allowfullscreen
-	/>
+	<div class="video-background">
+		<embed
+			src="https://www.youtube.com/embed/3z37Iy3JvB0?autohide=1&autoplay=1&mute=1"
+			wmode="transparent"
+			type="video/mp4"
+			width="100%"
+			height="100%"
+			allow="autoplay; encrypted-media; picture-in-picture"
+			allowfullscreen
+			title="Tabăra de leadership pentru profesori și instructori de dezbateri (ediția 2018)"
+		/>
+	</div>
 </div>
 
 <style>
 	.container {
 		width: 100%;
 		height: 100%;
-		background-color: #51b4c0;
+		background-color: var(--color-octonary);
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 	}
+
+	.video-background {
+		position: relative;
+		overflow: hidden;
+		width: 100vw;
+		height: 100vh;
+		pointer-events: none;
+	}
+
+	.video-background embed {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		width: 100vw;
+		height: 100vh;
+		transform: translate(-50%, -50%);
+	}
+
+	@media (min-aspect-ratio: 16/9) {
+		.video-background embed {
+			height: 56.25vw;
+		}
+	}
+
+	@media (max-aspect-ratio: 16/9) {
+		.video-background embed {
+			width: 177.78vh;
+		}
+	}
+
 	.columns {
 		width: 100%;
 		height: 100%;
@@ -56,76 +88,84 @@
 		text-align: center;
 		grid-template-columns: 1fr 1fr;
 	}
+
 	.column h3 {
 		font-size: 1.4rem;
 		margin: 0.6rem;
 	}
+
 	.column p {
 		margin: 0.4rem;
 		font-size: 1rem;
 	}
-	.video {
-		display: none;
-	}
+
 	.container h1 {
 		color: var(--color-secondary);
 		font-size: 2rem;
 		text-align: center;
+		font-weight: 800;
 		margin: 2rem;
 	}
+
 	@media screen and (min-width: 680px) {
 		.container {
 			position: relative;
 		}
-		.video {
-			display: block;
-			height: 50%;
-			width: 100%;
-			pointer-events: none;
-		}
+
 		.columns {
 			display: flex;
 			height: 40%;
 			width: 95%;
 			margin-bottom: 1rem;
 		}
+
 		.iconify {
 			margin: 1rem 1.2rem;
 		}
+
 		.column h3 {
 			font-size: 1.4rem;
 			margin: 1rem;
 			letter-spacing: 1px;
 		}
+
 		.column p {
 			letter-spacing: 1px;
 		}
 	}
+
 	@media screen and (min-width: 1024px) {
 		.container h1 {
 			font-size: 2.6rem;
 		}
+
 		.columns {
 			height: 50%;
 			width: 90%;
 		}
+
 		.column {
 			margin: 0 2rem;
 		}
+
 		.column p {
 			font-size: 1.2rem;
 		}
+
 		.column h3 {
 			font-size: 1.8rem;
 		}
 	}
+
 	@media screen and (min-width: 1440px) {
 		.columns {
 			width: 80%;
 		}
+
 		.column p {
 			font-size: 1.4rem;
 		}
+
 		.column h3 {
 			font-size: 2rem;
 		}
