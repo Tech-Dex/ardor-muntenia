b5109f42b06399e7020ce8a1981d9b3122ac3a78
Tech-Dex <rominu.alexandru@yahoo.com>
2023-03-21 19:23:55 +0200

    chore: ui update & read calendar from google drive

--- src/routes/dezbateri/calendar/+page.server.js
+++ src/routes/dezbateri/calendar/+page.server.js
@@ -1,7 +1,28 @@
+import { parse } from "csv-parse/sync";
+
 export async function load() {
 	const response = await fetch(
-		"https://raw.githubusercontent.com/Tech-Dex/ardor-muntenia/master/src/lib/data/calendar.json",
+		"https://docs.google.com/spreadsheets/d/1tVl_gnvAe-xQxec8np15XkJf4yLx9sCd-UL_lHLz91s/gviz/tq?tqx=out:csv&sheet=Sheet1",
 	);
-	const data = await response.json();
+	const csv = await response.text();
+
+	const records = parse(csv, {
+		columns: true,
+		skip_empty_lines: true,
+	});
+
+	const data = records.map((record) => {
+		return {
+			start: record["Start"],
+			end: record["End"],
+			titleHTML: record["Title"],
+			extendedProps: {
+				eventLink: record["Event Link"],
+			},
+			color: record["Color"],
+			allDay: record["All Day"] === "TRUE",
+		};
+	});
+	console.log(data);
 	return { data };
 }
